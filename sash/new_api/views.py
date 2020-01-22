f309179474a5b89ae06571047bbf8cdef2ae5dfa
Butch Paolo Madahan <butchpaolom@gmail.com>
2020-01-22 16:58:48 +0800

    stable

--- sash/new_api/views.py
+++ sash/new_api/views.py
@@ -83,7 +83,7 @@ class CustomerInfoView(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        if return_data:
+        if not return_data:
             return_data = serializer.data
         return Response(return_data, status=status.HTTP_201_CREATED, headers=headers)
     
